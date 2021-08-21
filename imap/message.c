bd430d0905e9a1dab4ce5fdec2b284b268245893
Richard Russon <rich@flatcap.org>
2021-08-21 12:21:25 -0700

    Reset msn_begin on a QRESYNC failure retry
    Since the msn_index will be regenerated differently, NeoMutt needs to
    scan for holes again.
    
    Upstream-commit: https://gitlab.com/muttmua/mutt/commit/44f2619c0932cc78575e70f99f1ba546e065e65e
    
    Co-authored-by: Pieter-Tjerk de Boer <p.t.deboer@utwente.nl>

--- imap/message.c
+++ imap/message.c
@@ -1318,6 +1318,7 @@ int imap_read_headers(struct Mailbox *m, unsigned int msn_begin,
   unsigned long long *pmodseq = NULL;
   unsigned long long hc_modseq = 0;
   char *uid_seqset = NULL;
+  unsigned int msn_begin_original = msn_begin;
 #endif /* USE_HCACHE */
 
   struct ImapAccountData *adata = imap_adata_get(m);
@@ -1440,6 +1441,7 @@ retry:
       FREE(&uid_seqset);
       uidvalidity = NULL;
       uid_next = 0;
+      msn_begin = msn_begin_original;
 
       goto retry;
     }
